7bda674c6c27d321c5075a1f4dbdef20f0379e49
Aparna1407 <aparnadesai7@gmail.com>
2024-10-17 16:01:49 +0530

    M21

---
+++ Conversation_with_string.py
@@ -0,0 +1,13 @@
+# create a program to concatenate multiple sentences
+
+# Take two strings as input from user
+print("Enter the First String: ")
+strOne = input()
+
+print("Enter the Second String: ")
+strTwo = input()
+
+# Concatenate the two strings
+# Using Addition Operator
+strThree = strOne + strTwo
+print("\nConcatenated String: ", strThree)
---
+++ Operators.py
@@ -0,0 +1,18 @@
+a = int(input("Enter a number"))
+b = int(input("Enter a second number"))
+
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+quo = a // b
+rem = a % b
+exp = a ** b
+
+print("The addition is", add)
+print("The subtraction is", sub)
+print("The multiplication is", mul)
+print("The division is", div)
+print("The quotient is", quo)
+print("The remainder is", rem)
+print("The exponent is", exp)
